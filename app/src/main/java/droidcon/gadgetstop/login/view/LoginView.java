41e9491d26aa0a0678376f74b02a8bb7a97fe90a
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-12-13 23:46:10 +0530

    Renaming the variable to follow the camelCase convention.

--- app/src/main/java/droidcon/gadgetstop/login/view/LoginView.java
+++ app/src/main/java/droidcon/gadgetstop/login/view/LoginView.java
@@ -14,5 +14,5 @@ public interface LoginView {
 
   void navigateToShoppingActivity();
 
-  void showTechnicalDifficultyError(int technical_difficulty);
+  void showTechnicalDifficultyError(int technicalDifficulty);
 }
